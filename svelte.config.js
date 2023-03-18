789489fbcae590ff0a9df61da143a40093166f64
Manu <manujsdev@gmail.com>
2023-03-18 17:15:28 -0400

    feat(adapter): change to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,9 @@ const config = {
   preprocess: [preprocess({ preserve: ['ld+json'], postcss: true }), mdsvex(mdsvexConfig)],
 
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      runtime: 'nodejs18.x'
+    })
   }
 };
 
